9214cd2d4306216a9a46148e64a31501e4a10c91
2200031921 <143632824+SivaSankarVaraPrasad@users.noreply.github.com>
2025-05-22 12:21:52 +0530

    Add files via upload

---
+++ Afford Medical/backend/src/main/java/com/affordmedical/backend/model/NumbersResponse.java
@@ -0,0 +1,18 @@
+package com.affordmedical.backend.model;
+
+
+import java.util.List;
+
+public class NumbersResponse {
+    private List<Integer> numbers;
+
+    // Getter and Setter for the 'numbers' field
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
+
---
+++ Afford Medical/backend/src/main/java/com/affordmedical/backend/service/CalService.java
@@ -0,0 +1,128 @@
+package com.affordmedical.backend.service;
+
+import java.util.List;
+import java.util.OptionalDouble;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.affordmedical.backend.model.NumbersResponse;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+@Service
+public class CalService {
+
+     @Autowired
+     private final RestTemplate restTemplate;
+
+    public CalService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public double primes(){
+        String accessKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ3ODk1ODM0LCJpYXQiOjE3NDc4OTU1MzQsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjMxYjYzZjExLWIxNjctNGQzNi04MzBjLTdjZWFkNmM0YjU2OCIsInN1YiI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSJ9LCJlbWFpbCI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSIsIm5hbWUiOiJ5YXJyYW1zZXR0aSBzaXZhIHNhbmthcmEgdmFyYSBwcmFzZCIsInJvbGxObyI6IjIyMDAwMzE5MjEiLCJhY2Nlc3NDb2RlIjoiYmVUSmpKIiwiY2xpZW50SUQiOiIzMWI2M2YxMS1iMTY3LTRkMzYtODMwYy03Y2VhZDZjNGI1NjgiLCJjbGllbnRTZWNyZXQiOiJHWlJ1VlhWTVNFcnVHblBWIn0.Egq1VIa3o94m7HaIwx3ZVQWuyFXAaiwq2wtzinTsWfI";
+        String url = "http://20.244.56.144/evaluation-service/primes";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessKey);
+          HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Fetch the data from the API
+        ResponseEntity<NumbersResponse> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entity,
+            NumbersResponse.class
+        );
+
+        // Extract numbers from the response body
+        List<Integer> numbers = response.getBody().getNumbers();
+        
+       OptionalDouble average = numbers.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .average();
+
+        return average.orElseThrow(() -> new IllegalStateException("Could not calculate the average"));
+    }
+
+    public double even(){
+        String accessKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ3ODk2NjExLCJpYXQiOjE3NDc4OTYzMTEsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjMxYjYzZjExLWIxNjctNGQzNi04MzBjLTdjZWFkNmM0YjU2OCIsInN1YiI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSJ9LCJlbWFpbCI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSIsIm5hbWUiOiJ5YXJyYW1zZXR0aSBzaXZhIHNhbmthcmEgdmFyYSBwcmFzZCIsInJvbGxObyI6IjIyMDAwMzE5MjEiLCJhY2Nlc3NDb2RlIjoiYmVUSmpKIiwiY2xpZW50SUQiOiIzMWI2M2YxMS1iMTY3LTRkMzYtODMwYy03Y2VhZDZjNGI1NjgiLCJjbGllbnRTZWNyZXQiOiJHWlJ1VlhWTVNFcnVHblBWIn0.s8qnMqZucGrLLJNQGhXzsq7ZSCZ1u1q-C6zHigsca6U";
+        String url = "http://20.244.56.144/evaluation-service/even";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessKey);
+          HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Fetch the data from the API
+        ResponseEntity<NumbersResponse> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entity,
+            NumbersResponse.class
+        );
+
+        // Extract numbers from the response body
+        List<Integer> numbers = response.getBody().getNumbers();
+        
+       OptionalDouble average = numbers.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .average();
+
+        return average.orElseThrow(() -> new IllegalStateException("Could not calculate the average"));
+    }
+
+    public double fib(){
+        String accessKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ3ODk2NzUyLCJpYXQiOjE3NDc4OTY0NTIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjMxYjYzZjExLWIxNjctNGQzNi04MzBjLTdjZWFkNmM0YjU2OCIsInN1YiI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSJ9LCJlbWFpbCI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSIsIm5hbWUiOiJ5YXJyYW1zZXR0aSBzaXZhIHNhbmthcmEgdmFyYSBwcmFzZCIsInJvbGxObyI6IjIyMDAwMzE5MjEiLCJhY2Nlc3NDb2RlIjoiYmVUSmpKIiwiY2xpZW50SUQiOiIzMWI2M2YxMS1iMTY3LTRkMzYtODMwYy03Y2VhZDZjNGI1NjgiLCJjbGllbnRTZWNyZXQiOiJHWlJ1VlhWTVNFcnVHblBWIn0.gpTxHBjj7HJKNxkw3QwvCP1MXe0ZSXZymL418bD4gOYU";
+        String url = "http://20.244.56.144/evaluation-service/fibo";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessKey);
+          HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Fetch the data from the API
+        ResponseEntity<NumbersResponse> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entity,
+            NumbersResponse.class
+        );
+
+        // Extract numbers from the response body
+        List<Integer> numbers = response.getBody().getNumbers();
+        
+       OptionalDouble average = numbers.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .average();
+
+        return average.orElseThrow(() -> new IllegalStateException("Could not calculate the average"));
+    }
+
+
+    public double rand(){
+        String accessKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ3ODk1ODM0LCJpYXQiOjE3NDc4OTU1MzQsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjMxYjYzZjExLWIxNjctNGQzNi04MzBjLTdjZWFkNmM0YjU2OCIsInN1YiI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSJ9LCJlbWFpbCI6IjIyMDAwMzE5MjFjc2VoQGdtYWlsLmNvbSIsIm5hbWUiOiJ5YXJyYW1zZXR0aSBzaXZhIHNhbmthcmEgdmFyYSBwcmFzZCIsInJvbGxObyI6IjIyMDAwMzE5MjEiLCJhY2Nlc3NDb2RlIjoiYmVUSmpKIiwiY2xpZW50SUQiOiIzMWI2M2YxMS1iMTY3LTRkMzYtODMwYy03Y2VhZDZjNGI1NjgiLCJjbGllbnRTZWNyZXQiOiJHWlJ1VlhWTVNFcnVHblBWIn0.Egq1VIa3o94m7HaIwx3ZVQWuyFXAaiwq2wtzinTsWfI";
+        String url = "http://20.244.56.144/evaluation-service/rand";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessKey);
+          HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Fetch the data from the API
+        ResponseEntity<NumbersResponse> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entity,
+            NumbersResponse.class
+        );
+
+        // Extract numbers from the response body
+        List<Integer> numbers = response.getBody().getNumbers();
+        
+       OptionalDouble average = numbers.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .average();
+
+        return average.orElseThrow(() -> new IllegalStateException("Could not calculate the average"));
+    }
+
+}
